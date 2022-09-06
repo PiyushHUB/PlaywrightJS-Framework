3144301ede5bb5ff727074475479f00efd61ad3d
PiyushHUB <87277876+PiyushHUB@users.noreply.github.com>
2022-09-06 17:47:12 +0530

    Add files via upload

---
+++ playwright.config.ts
@@ -0,0 +1,27 @@
+// @ts-check
+
+/** @type {import('@playwright/test').PlaywrightTestConfig} */
+const config = {
+    name: 'Microsoft Edge',
+    use: {
+        baseURL: 'http://localhost:3000',
+        //browserName: 'chromium',
+        channel: 'msedge',
+        headless: false,
+        viewport: { width: 1280, height: 720 },
+        ignoreHTTPSErrors: true,
+        video: 'on-first-retry',
+        screenshot: 'only-on-failure',
+        trace: 'retain-on-failure'
+    },    
+    testDir: 'tests',
+    timeout: 600000,
+    expect: {
+        timeout: 10 * 10000,
+    },
+    reporter: [ ['junit', { outputFile: 'results.xml' }] ],
+                //['html', { outputFolder: 'my-report' }] ],
+    workers: 10
+  };
+  
+  module.exports = config;
---
+++ test.js
@@ -0,0 +1,13 @@
+const playwright=require('playwright');
+
+(async() =>{
+  
+    const browser=await playwright["chromium"].launch({
+        headless:false
+    });
+    const context =await browser.newContext();
+    const page=await context.newPage();
+    await page.goto("https://google.com")
+    
+
+})();
