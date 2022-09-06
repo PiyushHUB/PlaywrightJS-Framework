5862aea51746e2769a3a5d918fee17b2c0d04822
PiyushHUB <87277876+PiyushHUB@users.noreply.github.com>
2022-09-06 17:58:53 +0530

    Add files via upload

---
+++ tests/Smaple.spec.js
@@ -0,0 +1,15 @@
+const { test, expect } = require('@playwright/test');
+const { Demo } = require('../Pageobjects/Demo');
+
+/*test.describe.configure({ mode: 'parallel' });*/
+
+test('Registration Validation9', async ({ page }) => {
+    const demo = new Demo(page);
+    await demo.Register();
+   /* await expect(demo.MMBTitle_div).toHaveText(['Manage myBusiness']);*/
+  });
+
+  test('Excel and api validation9', async ({ page }) => {
+    const demo = new Demo(page);
+    await demo.common_func();
+  });
